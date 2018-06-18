c95189f43d8993a149567c68443494605032479e
Cihan Karadag <ckaradag@orecx.com>
2018-06-18 13:57:51 -0400

    new class and factory system
    better way to disribute code to different plugins

--- orkbasecxx/SocketStreamer.cpp
+++ orkbasecxx/SocketStreamer.cpp
@@ -18,142 +18,17 @@ static LoggerPtr getLog() {
 	return s_log;
 }
 
-CStdString SocketStreamerConnection::ToString() {
-	return CStdString("");
-}
-
-void SocketStreamerConnection::ParseTarget(CStdString target) 
-{
-	CStdString ip;
-
-	ChopToken(ip,":",target);
-
-	if (!ACE_OS::inet_aton((PCSTR)ip, &m_addr.ip)) {
-		throw "Invalid host:" + ip ;
-	}
-
-	m_addr.port = strtol(target,NULL,0);
-	if (m_addr.port == 0) {
-		throw "Invalid  port:0";
-	}
-}
-
-/* SocketStreamerConnection::SocketStreamerConnection(struct in_addr & hostAddr, int port) { */
-/* 	m_addr.port = port; */
-/* 	memcpy(&m_addr.ip, &hostAddr, sizeof(struct in_addr)); */
-/* } */
-
-bool SocketStreamerConnection::Connect()
-{
-	char szIp[16];
-	ACE_INET_Addr server;
-	ACE_SOCK_Connector connector;
-
-	memset(m_buf, 0, sizeof(m_buf));
-
-	memset(&szIp, 0, sizeof(szIp));
-	ACE_OS::inet_ntop(AF_INET, (void*)&m_addr.ip, szIp, sizeof(szIp));
-
-	server.set(m_addr.port, szIp);
-	if(connector.connect(m_peer, server) == -1) {
-		return false;
-	}
-	return Init();
-}
-
-void SocketStreamerConnection::Close() {
-	m_peer.close();
-}
-
-size_t SocketStreamerConnection::Recv() {
-	CStdString logMsg;
-	m_bytesRead = m_peer.recv(m_buf, sizeof(m_buf));
-
-	if (m_bytesRead>0) {
-		ProcessData();
-		FLOG_INFO(getLog(),"DATA : %d",m_bytesRead);
-	}
-	return m_bytesRead;
-}
-
-void SocketStreamerConnection::ProcessData() {
-}
-
-bool SocketStreamerConnection::Init() {
-	return true; // default no handshake
-}
-
-SocketStreamerConnection* SocketStreamerConnection::Create(CStdString target) 
-{
-	CStdString protocol("mitel3000"); // use this if nothing is specified
-	ChopToken(protocol,"://",target);
-	protocol.ToLower();
-
-	if (s_factoryMap.find(protocol) == s_factoryMap.end()) {
-		throw "Could not find protocol : " + protocol;
-	}
-	return s_factoryMap[protocol]->create(target);
-}
-
-void SocketStreamerConnection::RegisterType(CStdString typeName, SocketStreamerConnectionFactory *factory) {
-	s_factoryMap[typeName] = factory;
-}
-
-std::map<CStdString,SocketStreamerConnectionFactory*> SocketStreamerConnection::s_factoryMap;
-
-// ------------------------
-
-int *t = new int;
-SocketStreamerConnection* CreateSocketStreamer(CStdString target)
-{
-	CStdString logMsg;
-	/* struct in_addr hostAddr; */
-
-
-	/* CStdString ip; */
-	/* int port; */
-	/* CStdString pass; */
-
-	/* ParseSocketStreamerTarget(target,ip,port,protocol,pass); */
-
-	/* if (!ACE_OS::inet_aton((PCSTR)ip, &hostAddr)) { */
-	/* 	FLOG_ERROR(getLog(),"Invalid host:%s in target:%s -- check SocketStreamerTargets in config.xml", ip); */
-	/* 	return NULL; */
-	/* } */
-
-	/* if (port == 0) { */
-	/* 	FLOG_ERROR(getLog(),"Invalid  port:%d in target:%s -- check SocketStreamerTargets in config.xml", port); */
-	/* 	return NULL; */
-	/* } */
-
-	/* SocketStreamerConnection * ssc = NULL; */
-
-	/* if (!ssc && Mitel3000Connection::DoesAcceptProtocol(protocol)) { */
-	/* 	ssc = new Mitel3000Connection(hostAddr,port); */
-	/* } */
-
-	/* if (!ssc && Mitel5000Connection::DoesAcceptProtocol(protocol)) { */
-	/* 	ssc = new Mitel5000Connection(hostAddr,port,pass); */
-	/* } */
-
-	/* if (ssc) { */
-	/* 	ssc->m_target = target; */
-	/* 	return ssc; */
-	/* } */
-
-	/* FLOG_ERROR(getLog(),"Unsupported protocol: %s -- check SocketStreamerTargets in config.xml", protocol); */
-	return NULL;
-}
-
-
-/* void SocketStreamer::ThreadHandler(void *args) */
-void ThreadHandler(void *args)
+void SocketStreamer::ThreadHandler(void *args)
 {
 	SetThreadName("orka:sockstream");
 
-	SocketStreamerConnection * ssc = (SocketStreamerConnection *) args; 
 	CStdString logMsg;
-	CStdString ipPort = ssc->m_target;
+	SocketStreamer* ssc = (SocketStreamer*) args; 
+
+	CStdString params = ssc->m_logMsg;
+	FLOG_INFO(getLog(), "Succesfully created thread (%s)", params);
+
+	CStdString ipPort = params;
 	bool connected = false;
 	time_t lastLogTime = 0;
 	int bytesRead = 0;
@@ -192,25 +67,255 @@ void ThreadHandler(void *args)
 	}
 }
 
-void SocketStreamer::Initialize()
+bool SocketStreamer::Parse(CStdString target) 
+{
+	CStdString ip;
+	ChopToken(ip,":",target);
+
+	if (!ACE_OS::inet_aton((PCSTR)ip, &m_addr.ip)) {
+		m_logMsg.Format("Invalid host:%s", ip);
+		return false;
+	}
+	m_logMsg += CStdString(" host:") + ip;
+
+	m_addr.port = strtol(target,NULL,0);
+	if (m_addr.port == 0) {
+		m_logMsg.Format("Invalid port:%s", target);
+		return false;
+	}
+	m_logMsg += CStdString(" port:") + target;
+
+	return true;
+}
+
+bool SocketStreamer::Connect()
+{
+	char szIp[16];
+	ACE_INET_Addr server;
+	ACE_SOCK_Connector connector;
+
+	memset(m_buf, 0, sizeof(m_buf));
+
+	memset(&szIp, 0, sizeof(szIp));
+	ACE_OS::inet_ntop(AF_INET, (void*)&m_addr.ip, szIp, sizeof(szIp));
+
+	server.set(m_addr.port, szIp);
+	if(connector.connect(m_peer, server) == -1) {
+		return false;
+	}
+	return Handshake();
+}
+
+void SocketStreamer::Close() {
+	m_peer.close();
+}
+
+size_t SocketStreamer::Recv() {
+	CStdString logMsg;
+	m_bytesRead = m_peer.recv(m_buf, sizeof(m_buf));
+
+	if (m_bytesRead>0) {
+		ProcessData();
+		FLOG_INFO(getLog(),"DATA : %d",m_bytesRead);
+	}
+	return m_bytesRead;
+}
+
+void SocketStreamer::ProcessData() {
+	// default do nothing
+}
+
+bool SocketStreamer::Handshake() {
+	return true; // default no handshake
+}
+
+void SocketStreamer::Initialize(SocketStreamerFactory *factory)
 {
 	CStdString logMsg;
+	SocketStreamerFactory ssf;
+
+	if (factory == NULL) {
+		factory = &ssf;
+	}
 
 	for (std::list<CStdString>::iterator it = CONFIG.m_socketStreamerTargets.begin(); it != CONFIG.m_socketStreamerTargets.end(); it++)
 	{
 		CStdString target=*it;
 
-		try 
-		{
-			SocketStreamerConnection * ssc = SocketStreamerConnection::Create(target);
-			if (!ACE_Thread_Manager::instance()->spawn(ACE_THR_FUNC(ThreadHandler), (void*)ssc)) {
-				FLOG_WARN(getLog(),"Failed to start thread on %s", target);
+		CStdString protocol;
+		ChopToken(protocol,"://",target);
+		protocol.ToLower();
+
+		if (factory->Accepts(protocol)) {
+
+			SocketStreamer *ss = factory->Create();
+			ss->m_logMsg.Format("protocol:%s",protocol);
+
+			if (!ss->Parse(target) || !ss->Spawn()) {
+				FLOG_ERROR(getLog(),"%s - %s", *it, ss->m_logMsg);
+				delete ss;
 			}
-		}
-		catch(CStdString s) 
-		{
-		/* FLOG_ERROR(getLog(),"Invalid  port:%d in target:%s -- check SocketStreamerTargets in config.xml", port); */
-			// log the error
 		}
 	}
 }
+
+bool SocketStreamer::Spawn() 
+{
+	if (!ACE_Thread_Manager::instance()->spawn(ACE_THR_FUNC(ThreadHandler), (void*)this)) {
+		m_logMsg = "Failed to start thread";
+		return false;
+	}
+	return true;
+}
+
+/* void SocketStreamer::Initialize(CSt) */
+/* { */
+/* 	CStdString logMsg; */
+
+/* 	for (std::list<CStdString>::iterator it = CONFIG.m_socketStreamerTargets.begin(); it != CONFIG.m_socketStreamerTargets.end(); it++) */
+/* 	{ */
+/* 		CStdString target=*it; */
+
+/* 		try */ 
+/* 		{ */
+/* 			SocketStreamerConnection * ssc = SocketStreamerConnection::Create(target); */
+/* 			if (!ACE_Thread_Manager::instance()->spawn(ACE_THR_FUNC(ThreadHandler), (void*)ssc)) { */
+/* 				FLOG_WARN(getLog(),"Failed to start thread on %s", target); */
+/* 			} */
+/* 		} */
+/* 		catch(CStdString s) */ 
+/* 		{ */
+/* 		/1* FLOG_ERROR(getLog(),"Invalid  port:%d in target:%s -- check SocketStreamerTargets in config.xml", port); *1/ */
+/* 			// log the error */
+/* 		} */
+/* 	} */
+/* } */
+
+
+
+/* CStdString SocketStreamerConnection::ToString() { */
+/* 	return CStdString(""); */
+/* } */
+
+/* void SocketStreamerConnection::ParseTarget(CStdString target) */ 
+/* { */
+/* 	CStdString ip; */
+
+/* 	ChopToken(ip,":",target); */
+
+/* 	if (!ACE_OS::inet_aton((PCSTR)ip, &m_addr.ip)) { */
+/* 		throw "Invalid host:" + ip ; */
+/* 	} */
+
+/* 	m_addr.port = strtol(target,NULL,0); */
+/* 	if (m_addr.port == 0) { */
+/* 		throw "Invalid  port:0"; */
+/* 	} */
+/* } */
+
+/* /1* SocketStreamerConnection::SocketStreamerConnection(struct in_addr & hostAddr, int port) { *1/ */
+/* /1* 	m_addr.port = port; *1/ */
+/* /1* 	memcpy(&m_addr.ip, &hostAddr, sizeof(struct in_addr)); *1/ */
+/* /1* } *1/ */
+
+/* bool SocketStreamerConnection::Connect() */
+/* { */
+/* 	char szIp[16]; */
+/* 	ACE_INET_Addr server; */
+/* 	ACE_SOCK_Connector connector; */
+
+/* 	memset(m_buf, 0, sizeof(m_buf)); */
+
+/* 	memset(&szIp, 0, sizeof(szIp)); */
+/* 	ACE_OS::inet_ntop(AF_INET, (void*)&m_addr.ip, szIp, sizeof(szIp)); */
+
+/* 	server.set(m_addr.port, szIp); */
+/* 	if(connector.connect(m_peer, server) == -1) { */
+/* 		return false; */
+/* 	} */
+/* 	return Init(); */
+/* } */
+
+/* void SocketStreamerConnection::Close() { */
+/* 	m_peer.close(); */
+/* } */
+
+/* size_t SocketStreamerConnection::Recv() { */
+/* 	CStdString logMsg; */
+/* 	m_bytesRead = m_peer.recv(m_buf, sizeof(m_buf)); */
+
+/* 	if (m_bytesRead>0) { */
+/* 		ProcessData(); */
+/* 		FLOG_INFO(getLog(),"DATA : %d",m_bytesRead); */
+/* 	} */
+/* 	return m_bytesRead; */
+/* } */
+
+/* void SocketStreamerConnection::ProcessData() { */
+/* } */
+
+/* bool SocketStreamerConnection::Init() { */
+/* 	return true; // default no handshake */
+/* } */
+
+/* SocketStreamerConnection* SocketStreamerConnection::Create(CStdString target) */ 
+/* { */
+/* 	CStdString protocol("mitel3000"); // use this if nothing is specified */
+/* 	ChopToken(protocol,"://",target); */
+/* 	protocol.ToLower(); */
+
+/* 	if (s_factoryMap.find(protocol) == s_factoryMap.end()) { */
+/* 		throw "Could not find protocol : " + protocol; */
+/* 	} */
+/* 	return s_factoryMap[protocol]->create(target); */
+/* } */
+
+/* void SocketStreamerConnection::RegisterType(CStdString typeName, SocketStreamerConnectionFactory *factory) { */
+/* 	s_factoryMap[typeName] = factory; */
+/* } */
+
+/* std::map<CStdString,SocketStreamerConnectionFactory*> SocketStreamerConnection::s_factoryMap; */
+
+/* // ------------------------ */
+
+/* SocketStreamerConnection* CreateSocketStreamer(CStdString target) */
+/* { */
+/* 	CStdString logMsg; */
+/* 	/1* struct in_addr hostAddr; *1/ */
+
+
+/* 	/1* CStdString ip; *1/ */
+/* 	/1* int port; *1/ */
+/* 	/1* CStdString pass; *1/ */
+
+/* 	/1* ParseSocketStreamerTarget(target,ip,port,protocol,pass); *1/ */
+
+/* 	/1* if (!ACE_OS::inet_aton((PCSTR)ip, &hostAddr)) { *1/ */
+/* 	/1* 	FLOG_ERROR(getLog(),"Invalid host:%s in target:%s -- check SocketStreamerTargets in config.xml", ip); *1/ */
+/* 	/1* 	return NULL; *1/ */
+/* 	/1* } *1/ */
+
+/* 	/1* if (port == 0) { *1/ */
+/* 	/1* 	FLOG_ERROR(getLog(),"Invalid  port:%d in target:%s -- check SocketStreamerTargets in config.xml", port); *1/ */
+/* 	/1* 	return NULL; *1/ */
+/* 	/1* } *1/ */
+
+/* 	/1* SocketStreamerConnection * ssc = NULL; *1/ */
+
+/* 	/1* if (!ssc && Mitel3000Connection::DoesAcceptProtocol(protocol)) { *1/ */
+/* 	/1* 	ssc = new Mitel3000Connection(hostAddr,port); *1/ */
+/* 	/1* } *1/ */
+
+/* 	/1* if (!ssc && Mitel5000Connection::DoesAcceptProtocol(protocol)) { *1/ */
+/* 	/1* 	ssc = new Mitel5000Connection(hostAddr,port,pass); *1/ */
+/* 	/1* } *1/ */
+
+/* 	/1* if (ssc) { *1/ */
+/* 	/1* 	ssc->m_target = target; *1/ */
+/* 	/1* 	return ssc; *1/ */
+/* 	/1* } *1/ */
+
+/* 	/1* FLOG_ERROR(getLog(),"Unsupported protocol: %s -- check SocketStreamerTargets in config.xml", protocol); *1/ */
+/* 	return NULL; */
+/* } */
+
--- orkbasecxx/SocketStreamer.h
+++ orkbasecxx/SocketStreamer.h
@@ -24,65 +24,98 @@
 #include "ace/SOCK_Stream.h"
 #include "ace/OS_NS_unistd.h"
 
-
-class SocketStreamerConnectionFactory;
-
-class SocketStreamerConnection {
-	public:
-		static SocketStreamerConnection* Create(CStdString target);
-		void ParseTarget(CStdString target);
-
-		CStdString m_target;
-		TcpAddress m_addr;
-		ACE_SOCK_Stream m_peer;
-		char m_buf[1024];
-		size_t m_bytesRead;
-
-		/* SocketStreamerConnection(struct in_addr & hostAddr, int port); */
-
-		bool Connect();
-		void Close();
-		size_t Recv();
-
-		virtual CStdString ToString();
-
-		static void RegisterType(CStdString typeName, SocketStreamerConnectionFactory *factory);
-
-	protected:
-		virtual void ProcessData();
-		virtual bool Init();
-
-		static std::map<CStdString, SocketStreamerConnectionFactory*> s_factoryMap;
-
-		SocketStreamerConnection(CStdString target);
-		SocketStreamerConnection() {}
-	private:
-};
-
-class SocketStreamerConnectionFactory
-{
-public:
-    virtual SocketStreamerConnection *create(CStdString target) = 0;
-};
-
-#define REGISTER_SOCKET_STREAMER_CONNECTION_TYPE(klass) \
-    class klass##Factory : public SocketStreamerConnectionFactory { \
-    public: \
-        klass##Factory() \
-        { \
-            SocketStreamerConnection::RegisterType(#klass, this); \
-        } \
-        virtual SocketStreamerConnection *create(CStdString t) { \
-            return new klass(t); \
-        } \
-    }; \
-    static klass##Factory global_##klass##Factory;
-	
+class SocketStreamerFactory;
 
 class DLL_IMPORT_EXPORT_ORKBASE SocketStreamer {
 public:
-	static void Initialize();
-	/* static void ThreadHandler(void *args); */
+	static void Initialize(SocketStreamerFactory *factory=NULL);
+
+protected:
+	SocketStreamer() {}
+	bool Connect();
+	void Close();
+	size_t Recv();
+	virtual void ProcessData();
+	virtual bool Handshake();
+	virtual bool Parse(CStdString target);
+
+	CStdString m_protocol;
+	CStdString m_logMsg;
+
+	TcpAddress m_addr;
+	ACE_SOCK_Stream m_peer;
+	char m_buf[1024];
+	size_t m_bytesRead;
+	static void ThreadHandler(void *args);
+
+	friend class SocketStreamerFactory;
+
+private:
+	bool Spawn();
+};
+
+class SocketStreamerFactory
+{
+protected:
+	SocketStreamerFactory() {}
+    virtual SocketStreamer* Create() { return new SocketStreamer(); } 
+	virtual bool Accepts(CStdString protocolName) {
+		return (protocolName == "");
+	}
+
+	friend class SocketStreamer;
 };
 
 #endif
+/* class SocketStreamerConnectionFactory; */
+
+/* class SocketStreamerConnection { */
+/* 	public: */
+/* 		static SocketStreamerConnection* Create(CStdString target); */
+/* 		void ParseTarget(CStdString target); */
+
+/* 		CStdString m_target; */
+/* 		TcpAddress m_addr; */
+/* 		ACE_SOCK_Stream m_peer; */
+/* 		char m_buf[1024]; */
+/* 		size_t m_bytesRead; */
+
+/* 		/1* SocketStreamerConnection(struct in_addr & hostAddr, int port); *1/ */
+
+/* 		bool Connect(); */
+/* 		void Close(); */
+/* 		size_t Recv(); */
+
+/* 		virtual CStdString ToString(); */
+
+/* 		static void RegisterType(CStdString typeName, SocketStreamerConnectionFactory *factory); */
+
+/* 	protected: */
+/* 		virtual void ProcessData(); */
+/* 		virtual bool Init(); */
+
+/* 		static std::map<CStdString, SocketStreamerConnectionFactory*> s_factoryMap; */
+
+/* 		SocketStreamerConnection(CStdString target); */
+/* 		SocketStreamerConnection() {} */
+/* 	private: */
+/* }; */
+
+/* class SocketStreamerConnectionFactory */
+/* { */
+/* public: */
+/*     virtual SocketStreamerConnection *create(CStdString target) = 0; */
+/* }; */
+
+/* #define REGISTER_SOCKET_STREAMER_CONNECTION_TYPE(klass) \ */
+/*     class klass##Factory : public SocketStreamerConnectionFactory { \ */
+/*     public: \ */
+/*         klass##Factory() \ */
+/*         { \ */
+/*             SocketStreamerConnection::RegisterType(#klass, this); \ */
+/*         } \ */
+/*         virtual SocketStreamerConnection *create(CStdString t) { \ */
+/*             return new klass(t); \ */
+/*         } \ */
+/*     }; \ */
+/*     static klass##Factory global_##klass##Factory; */
